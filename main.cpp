95bb86e32d9725dd39259a4d6cc3322b7397f568
yj <jnyngjn@naver.com>
2024-03-23 01:19:46 +0900

    v3

--- main.cpp
+++ main.cpp
@@ -10,16 +10,44 @@ int main(void)
 {
     int answer[3];
     initialize_seed();
-    for (int i = 0; i < 3; i++)
+    for (int i = 0, n; i < 3; i++)
     {
-        answer[i] = rand_0to9();
+        n = rand_0to9();
+        switch (i)
+        {
+        case 0:
+            answer[0] = n;
+            break;
+        case 1:
+            if (n == answer[0])
+            {
+                i--;
+                break;
+            } else
+            {
+                answer[1] = n;
+                break;
+            }
+        case 2:
+            if (n == answer[0] || n == answer[1])
+            {
+                i--;
+                break;
+            } else
+            {
+                answer[2] = n;
+                break;
+            }
+        default:
+            break;
+        }
     }
-    cout << "Answer is " << answer[0] << answer[1] << answer[2] << endl;
 
     int guess[3];
-    int strike_count, ball_count;
+    int strike_count, ball_count, chance_count = 5;
     while (true)
     {
+        cout << chance_count << " chances left." << endl;
         cout << "Enter a guess: ";
         for (int i = 0; i < 3; i++)
         {
@@ -30,12 +58,19 @@ int main(void)
         ball_count = 0;
 
         check(&strike_count, &ball_count, guess, answer);
+        chance_count--;
 
         if (3 <= strike_count)
         {
             cout << "You win!" << endl;
             break;
         }
+        if (chance_count <= 0)
+        {
+            cout << "You lose!" << endl;
+            break;
+        }
+
         cout << "Strikes: " << strike_count << ", Balls: " << ball_count << endl;
     }
 
